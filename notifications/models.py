453db8869c6113dced4e1afd114169185a57e30b
gujuluva-pixel <gujuluva.umich.edu>
2025-11-22 17:34:35 -0500

    email notification signal

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,6 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import send_mail
 from django.db import models
 from accounts.models import User
 
